1579432c5765ea9a66bd2d08de1779531d49f0cd
Mary Kaki <justonly4testing@gmail.com>
2021-06-14 14:29:28 +0300

    committing react todo list before redoing it into components.

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+function App() {
+
+  const[inputText, setInputText] = useState(""); // this state holds old and new input
+  const[items, setItems] = useState([]); // this state holds old and new list items for rendering.
+
+  //this function holds new list items
+  function handleChange(event){
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  //this function add new list item to the list using the spread feature
+  function addItem(){
+    setItems(prevItems =>{
+      return [...prevItems,inputText];
+    });
+    
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input 
+        onChange={handleChange}
+        type="text"
+        value={inputText}  />
+        <button onClick={addItem} >
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map(item => <li>{item}</li>) } 
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
